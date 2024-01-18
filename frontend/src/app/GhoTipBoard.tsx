65ea3b948fe1717c1c04aff9cf66b813908b0409
Ricy <ricyzhang951@gmail.com>
2024-01-18 12:25:18 -0800

    feat: GHO landing text

---
+++ frontend/src/app/GhoTipBoard.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import GHO from '@/assets/icons/GHO.png';
+
+const GhoTipBoard: React.FC = () => {
+  return (
+    <div className="pt-[26px] flex flex-col gap-y-[18px]">
+      <div className="flex flex-row gap-x-[8px]">
+        <Image
+          className="w-[22px] h-[30px]"
+          src={GHO}
+          width={22}
+          height={30}
+          alt="gho logo"
+        />
+        <div className="text-[24px] font-semibold">GHO Tip</div>
+      </div>
+      <div>
+        {/* <span>GHO Tip:</span> */}
+        Effortlessly pay tips with the assurance of privacy, safety, and
+        efficiency. Simplify your transactions now!
+      </div>
+    </div>
+  );
+};
+
+export default GhoTipBoard;
