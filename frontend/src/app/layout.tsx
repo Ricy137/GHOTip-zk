83586f0722be5b46c87ec9ab41133c0fceb744e8
Ricy <ricyzhang951@gmail.com>
2024-01-20 01:44:00 -0800

    fix: deploy issues

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Script from 'next/script';
 import { Inter } from 'next/font/google';
+import GHOIcon from '@/assets/icons/GHOTip.svg';
 import { ToastRender } from '@/components/Toast';
 import ModalRender from '@/components/Modal';
 import Providers from '@/modules/Providers';
@@ -10,8 +11,10 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'GHOTip',
+  applicationName: 'GHOTip',
   description:
-    ' Effortlessly pay tips with the assurance of privacy, safety, and efficiency. Simplify your transactions now!',
+    'Effortlessly pay tips with the assurance of privacy, safety, and efficiency. Simplify your transactions now!',
+  icons: GHOIcon,
 };
 
 export default function RootLayout({
@@ -21,7 +24,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script src="/js/snarkjs.min.js" />
+      <head>
+        <link rel="icon" href="./favicon.ico" sizes="any" />
+        <Script src="/js/snarkjs.min.js" />
+      </head>
+
       <body className={inter.className}>
         <Providers>
           <ToastRender />
