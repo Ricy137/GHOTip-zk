aa01dafe35645f963756941982402e0cdf874e22
Ricy <ricyzhang951@gmail.com>
2024-01-19 23:27:56 -0800

    feat: responsive

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import GHOTab from './GHOTab';
 
 export default function Home() {
   return (
-    <main className="pt-[123px] flex min-h-screen flex-col items-center justify-between">
-      <div className="grid grid-cols-2 justify-items-center gap-x-[43px] w-[65.3%]">
+    <main className="pt-[123px] flex min-h-screen flex-col items-center justify-between 2xl:justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 justify-items-center gap-[43px] md:w-3/4 lg:w-[65.3%] max-w-[1440px]">
         <div className="px-[13px]">
           <GhoTipBoard />
         </div>
